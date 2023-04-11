5a2b5f02106b09777108b01c0d66aa5274c180b2
krabecb <briankrabec@gmail.com>
2023-04-11 11:15:20 -0500

    Home route completed

--- catcollector/urls.py
+++ catcollector/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # At localhost:8000, we are allowing access to url paths specific to our main_app (Django app)
+    path('', include('main_app.urls'))
 ]
