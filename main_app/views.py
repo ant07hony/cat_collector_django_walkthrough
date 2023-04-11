ab3785163fc3fd41b030a1e4f1bda3eed65414aa
krabecb <briankrabec@gmail.com>
2023-04-11 12:53:42 -0500

    Completed cats index

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+# Define the about view
 def about(request):
     return render(request, 'about.html')
+
+# Define the index view
+def cats_index(request):
+    # Handles the request, points to the template to render, and we are providing a dict with a cats key which has a value of all the cats
+    return render(request, 'cats/index.html', { 'cats': cats })
