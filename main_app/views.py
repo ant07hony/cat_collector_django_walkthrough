69670aa6d8ac21f73a1932b77fbf1227af9d0133
krabecb <briankrabec@gmail.com>
2023-04-12 10:34:56 -0500

    Interacted with Django shell and created cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Cat
 
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# cats = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 # Create your views here.
 def home(request):
@@ -15,5 +16,6 @@ def about(request):
 
 # Define the index view
 def cats_index(request):
+    cats = Cat.objects.all()
     # Handles the request, points to the template to render, and we are providing a dict with a cats key which has a value of all the cats
     return render(request, 'cats/index.html', { 'cats': cats })
