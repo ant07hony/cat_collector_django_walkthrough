f3115291b6cf68a8d813f78e362088c148e63761
krabecb <briankrabec@gmail.com>
2023-04-13 10:42:50 -0500

    Added CreateView CBV

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('cats/', views.cats_index, name='index')
+    path('cats/', views.cats_index, name='index'),
+    path('cats/<int:cat_id>/', views.cats_detail, name='detail'),
+    path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Cat
 
 # cats = [
@@ -19,3 +20,16 @@ def cats_index(request):
     cats = Cat.objects.all()
     # Handles the request, points to the template to render, and we are providing a dict with a cats key which has a value of all the cats
     return render(request, 'cats/index.html', { 'cats': cats })
+
+# Define the detail view
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    # Special string pattern Django will use
+    # success_url = '/cats/{cat_id}' <- what it may look like under hood
+    # Or if you wanted to redirect to the index page
+    # success_url = '/cats'
