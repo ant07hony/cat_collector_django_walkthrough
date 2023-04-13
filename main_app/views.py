17633acfec4c91490e8eba45d98401d8b0d5817a
krabecb <briankrabec@gmail.com>
2023-04-13 13:37:51 -0500

    Completed one to many walkthrough

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 from .forms import FeedingForm
@@ -27,6 +27,19 @@ def cats_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form})
+
+def add_feeding(request, cat_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
+
 
 class CatCreate(CreateView):
     model = Cat
