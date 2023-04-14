df5f24e7d372c0693b1275d5382cd3c8c2e39927
krabecb <briankrabec@gmail.com>
2023-04-14 09:44:36 -0500

    Created many to many association and updated detail template

--- main_app/models.py
+++ main_app/models.py
@@ -8,11 +8,19 @@ MEALS = (
 )
 
 # Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
